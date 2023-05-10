b3e936e057ef07b504328918a5a1dccb7fc63bdb
Tero Kettunen <101104683+ErtoBoy@users.noreply.github.com>
2023-05-10 14:28:36 +0300

    Comment added
    Added comment to "EyeLook and "LightSwitch" scripts

--- Assets/Scripts/LightSwitch.cs
+++ Assets/Scripts/LightSwitch.cs
@@ -4,33 +4,37 @@ using UnityEngine;
 
 public class LightSwitch : MonoBehaviour
 {
-    public GameObject light;
-    public bool toggle;
-    public Animator switchAnim;
+    public GameObject light;// The game object representing the light to be turned on/off
+    public bool toggle;// A boolean value representing whether the light is on or off
+    public Animator switchAnim;// The animator component responsible for animating the light switch
 
 
-    public void LightOn()
+    public void LightOn()// Turn on the light and play an audio clip
     {
-        AudioManager.instance.PlaySFX("LightSwitch");
+        AudioManager.instance.PlaySFX("LightSwitch");// Play a sound effect using the AudioManager singleton
 
+        // Reset the "press" trigger on the animator and then trigger it again to play the animation
         switchAnim.ResetTrigger("press");
         switchAnim.SetTrigger("press");
 
+        // Activate the light game object
         light.SetActive(true);
-        if(toggle == false)
+
+
+        if(toggle == false)// If the toggle variable is currently false, set it to true in 0.5 seconds
         {
             Invoke("ToggleTime", 0.5f);
         }
     }
-    public void ToggleTime()
+    public void ToggleTime()// Helper method to set the toggle variable to true
     {
         toggle = true;
     }
 
-    public void LightOff()
+    public void LightOff() // Turn off the light if it is currently on
     {
        
-        if (toggle == true)
+        if (toggle == true)// If the toggle variable is true, deactivate the light and set the toggle variable to false
         {
             light.SetActive(false);
             toggle = false;
