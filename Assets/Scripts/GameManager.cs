616e4c1035b3b4aecee980889215f6900b0396b5
Tero Kettunen <101104683+ErtoBoy@users.noreply.github.com>
2023-05-24 08:52:50 +0300

    Update GameManager.cs
    Simplified code in script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,36 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    float oldTime = 0.0F;
-    float theDeltaTime = 0.0F;
-    float curTime = 0.0F;
-    float timeTaken = 0.0F;
-    public int frameRate = 60;
+    public int targetFrameRate = 60;
 
-    // Use this for initialization
-    void Start()
+    private void Start()
     {
-        theDeltaTime = (1.0F / frameRate);
-        oldTime = Time.realtimeSinceStartup;
-    }
-
-
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        curTime = Time.realtimeSinceStartup;
-        timeTaken = (curTime - oldTime);
-        if (timeTaken < theDeltaTime)
-        {
-            Thread.Sleep((int)(1000 * (theDeltaTime - timeTaken)));
-        }
-
-
-        oldTime = Time.realtimeSinceStartup;
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFrameRate;
     }
 
 }
